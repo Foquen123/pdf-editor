cd45c88e1d88bd1b33c2d3b69c67b446e030e7c9
Artem <rar19062005@mail.ru>
2026-05-21 21:46:39 +0700

    base path change

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,8 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-   allowedDevOrigins: ['192.168.9.3', 'localhost', '192.168.0.47', '*.local'],
-  
+  allowedDevOrigins: ['192.168.9.3', 'localhost', '192.168.0.47', '*.local'],
+  basePath: '/edit-pdf',
 };
 
 export default nextConfig;
