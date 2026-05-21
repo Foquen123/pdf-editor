5a5771223b63b6a4613d284829db32a59d4d3cc9
Artem <rar19062005@mail.ru>
2026-05-21 21:38:04 +0700

    port change

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
    allowedDevOrigins: ['192.168.9.3', 'localhost', '192.168.0.47', '*.local'],
+  
 };
 
 export default nextConfig;
